c08a963fe91adc66675f7a0e77898497e5ae5f21
Amin <reihani.eng@gmail.com>
2023-12-11 16:12:15 +0330

    Disable changeDatabase function

--- routes/web.php
+++ routes/web.php
@@ -23,33 +23,17 @@ use Illuminate\Support\Facades\Route;
 require __DIR__ . '/auth.php';
 
 
-Route::get('changeDatabase', function () {
+// Route::get('changeDatabase', function () {
 
-    // $all_users = App\Models\User::all();
-    // foreach ($all_users as $value) {
-    //     // FIX - ISSUE in USERNAME
-    //     if (str_contains($value->username, '-')) {
-    //         $username2 = str_replace("-", "_", $value->username);
-    //         $value->username = $username2;
-    //         $value->save();
-    //     }
+//     ini_set('max_execution_time', 1800);
+//     App\Models\Product::chunk(1000, function($products) {
+//         foreach ($products as $product) {
+//             $product_currency = $product->determine_product_currency();
+//             if($product_currency != null && $product_currency != "تومان") {
+//                 $product->trading_method = "export";
+//                 $product->save();
+//             }
+//         }
+//     });
 
-    //     // LOWER CASE USERNAME
-    //     $username2 = strtolower($value->username);
-    //     $value->username = $username2;
-    //     $value->save();
-    // }
-
-    ini_set('max_execution_time', 180);
-    App\Models\Product::chunk(1000, function($products) {
-        foreach ($products as $product) {
-            if($product->determine_product_currency() != null && $product->determine_product_currency() != "تومان") {
-            
-                $product->trading_method = "export";
-                $product->save();
-            }
-        }
-    });
-    
-
-});
+// });
