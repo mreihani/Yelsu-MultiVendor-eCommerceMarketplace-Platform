2cf3ef71ced2c4869406f10bac7d0012ab17c348
Amin <reihani.eng@gmail.com>
2024-02-01 22:45:55 +0330

    Fix update payment db

--- app/Http/Controllers/Frontend/PaymentController.php
+++ app/Http/Controllers/Frontend/PaymentController.php
@@ -154,7 +154,16 @@ class PaymentController extends Controller
 
            // If the transaction is successfull after verification.
             if($verifyTransactionSatus) {
-               
+                dd(
+                    [
+                        'refnumber' => $RefNum,
+                        'terminal_id' => $request->TerminalId,
+                        'trance_no' => $request->TraceNo,
+                        'amount' => $request->Amount,
+                        'rrn' => $request->Rrn,
+                        'secure_pan' => $request->SecurePan,
+                    ]
+                );
                 // Update the payment status
                 $payment->update([
                     'status' => 1,
